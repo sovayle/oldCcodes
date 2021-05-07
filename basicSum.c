3b4694e1eb4a4c855545a585637a8062c290d440
Amirul Azim <47412407+sovayle@users.noreply.github.com>
2021-05-07 13:24:51 +0800

    Create basicSum.c

---
+++ basicSum.c
@@ -0,0 +1,19 @@
+  
+#include <stdio.h>
+int main()
+{
+	
+	int no1,no2,sum;
+	printf("Enter number 1: ");
+	scanf("%d", &no1);
+	printf("Enter number 2: ");
+	scanf("%d", &no2);
+	
+	sum = no1 + no2;
+	
+	printf("The sum is %d", sum);
+	
+	system("pause");
+	return 0;
+	
+}
